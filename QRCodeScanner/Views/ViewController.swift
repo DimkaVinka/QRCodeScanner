d23d0fe1e24af961d06ae4f641c8cb3d3aa47d41
DimkaVinka <dimkavinka@gmail.com>
2022-05-26 09:01:00 +0300

    made AV UI

--- QRCodeScanner/Views/ViewController.swift
+++ QRCodeScanner/Views/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import AVFoundation
+
+protocol ViewProtocol: AnyObject {
+    func showAlert()
+    func dismiss()
+    func pushTo(controller: UIViewController)
+    func addLayer(layer: AVCaptureVideoPreviewLayer?)
+}
 
 class ViewController: UIViewController {
 
+    var presenter: PresenterProtocol?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.black
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter?.handleAppearigView()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        presenter?.test()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        presenter?.handleDisappearigView()
+    }
+}
 
+extension ViewController: ViewProtocol {
+    func addLayer(layer: AVCaptureVideoPreviewLayer?) {
+        guard let layer = layer else { return }
+        view.layer.addSublayer(layer)
+        layer.frame = view.layer.bounds
+    }
+
+    func pushTo(controller: UIViewController) {
+        navigationController?.pushViewController(controller, animated: true)
+    }
+
+    func dismiss() {
+        dismiss(animated: true, completion: nil)
+    }
+
+    func showAlert() {
+        let ac = UIAlertController(title: "Scanning not supported",
+                                   message: "Your device does not support scanning a code from an item. Please use a device with a camera.",
+                                   preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
 }
 
